c0468a65ec600959e433c4fdd6d7174f818e9a63
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-12-02 21:37:37 +0000

    set up SQL database

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Set the URI for your SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ultimate_planner.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional, but recommended to disable
+
+# Initialize SQLAlchemy
+db = SQLAlchemy(app)
+
+# Define the User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.id}', '{self.username}')"
+
 
 @app.route('/')
 def home():
